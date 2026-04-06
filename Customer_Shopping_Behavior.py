da6791129b793b9f55ef58f6bfd4a7e5639f849a
Pravallika-kolli <145193268+Pravallika-kolli@users.noreply.github.com>
2026-04-06 20:49:17 +0530

    Update Customer_Shopping_Behavior.py

--- Customer_Shopping_Behavior.py
+++ Customer_Shopping_Behavior.py
@@ -1,79 +1,29 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[4]:
-
-
 import pandas as pd
 
 df = pd.read_csv('customer_shopping_behavior.csv')
 
-
-# In[5]:
-
-
 df.head()
-
-
-# In[7]:
-
 
 df.info()
 
-
-# In[8]:
-
-
 df.describe(include='all')
 
-
-# In[9]:
-
-
 df.isnull().sum()
-
-
-# In[10]:
-
 
 df['Review Rating'] = df.groupby('Category')['Review Rating'].transform(lambda x: x.fillna(x.median()))
 
-
-# In[11]:
-
-
 df.isnull().sum()
-
-
-# In[12]:
-
 
 df.columns = df.columns.str.lower()
 df.columns = df.columns.str.replace(' ','_')
 df = df.rename(columns={'purchase_amount_(usd)':'purchase_amount'})
-
-
-# In[13]:
-
-
 df.columns
-
-
-# In[14]:
 
 
 # create a column for age_group
 labels = ['Young Adult', 'Adult', 'Middle-aged', 'Senior']
 df['age_group'] = pd.qcut(df['age'], q=4, labels = labels)
-
-
-# In[15]:
-
-
 df[['age','age_group']].head(10)
-
-
-# In[16]:
 
 
 # create column for purchase_frequency_days 
@@ -88,46 +38,17 @@ frequency_mapping = {
 }
 
 df['purchase_frequency_days'] = df['frequency_of_purchases'].map(frequency_mapping)
-
-
-# In[17]:
-
-
 df[['purchase_frequency_days','frequency_of_purchases']].head(10)
-
-
-# In[18]:
-
 
 df[['discount_applied','promo_code_used']].head(10)
 
-
-# In[19]:
-
-
 (df['discount_applied'] == df['promo_code_used']).all()
 
-
-# In[ ]:
-
-
 df = df.drop('promo_code_used', axis=1)
-
-
-# In[20]:
-
-
 df.columns
 
-
-# In[22]:
-
-
+#installing the required libraries
 pip install psycopg2.binary sqlalchemy
-
-
-# In[24]:
-
 
 from sqlalchemy import create_engine
 
@@ -143,10 +64,3 @@ table_name = "customer"
 df.to_sql(table_name, engine, if_exists="replace", index=False)
 
 print(f"Data successfully loaded into table '{table_name}' in database '{database}'.")
-
-
-# In[ ]:
-
-
-
-
